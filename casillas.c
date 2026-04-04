1a51e9dde3d6dc157c540d4ff03e23f93dce1be8
cchacon-rt <rebeca.citlali.torres@cua.uam.mx>
2026-04-03 21:40:43 -0600

    adicion de -1 para contnuar lectura de coordenadas

--- casillas.c
+++ casillas.c
@@ -1,169 +1,136 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
 #include <ctype.h>
 
-#define FILAS 12
-#define COLUMNAS 12
+#define VACIO   '#'
+#define ESPACIO '.'
 
-char lineas[20];
-char columnas[15];
-
-char car;
-char palabra[50];
-
-#define vacio '#'
-#define espacio '.'
-
-
-typedef struct
-{
+typedef struct {
     char valor;
     int usoContador;
 } T_casilla;
 
+T_casilla *tablero;
+int numLineas, numColumnas;
 
+char car;
+char palabra[50];
 
-//T_casilla tablero[FILAS][COLUMNAS];
-/*/////////////////////////////////////////////////////////////*/
-typedef enum { TRUE = 1, FALSE = 0 } T_bool;  //da valor de T o F segun un 1 o 0
+void inicializarTablero() {
+    for (int f = 0; f < numLineas; f++) {
+        for (int c = 0; c < numColumnas; c++) {
+            tablero[f * numColumnas + c].valor = ESPACIO;
+            tablero[f * numColumnas + c].usoContador = 0;
+        }
+    }
+}
 
+void imprimirTablero() {
+    for (int f = 0; f < numLineas; f++) {
+        for (int c = 0; c < numColumnas; c++) {
+            printf(" %c", tablero[f * numColumnas + c].valor);
+        }
+        printf("\n");
+    }
+}
 
-typedef enum{
-    HORIZONTAL = 1,
-    VERTICAL = 2
-}   T_sentido;
-
-typedef struct{
-    T_sentido sentido;
-    int x;
-    int y;
-    int len; //longitud
-    T_bool ocupado;
-} T_espacio;
-
-
-
-
-void adquirirPalabras (FILE *aStream)
-{
-    int i;
-
-    while (((car==' ')||(car=='\t')||(car=='\n')||(car=='\r') || (isdigit(car))) && (car!=EOF)) car = fgetc(aStream);
-        i = 0;
-
-    while (((car!=' ')&&(car!='\t')&&(car!='\n')&&(car!='\r') && !(isdigit(car))) && (car!=EOF))
-    {
+void adquirirPalabra(FILE *aStream) {
+    int i = 0;
+    while ((car==' ')||(car=='\t')||(car=='\n')||(car=='\r')||(isdigit(car))) {
+        car = fgetc(aStream);
+        if (car == EOF) {
+            palabra[0] = '\0';
+            return;
+        }
+    }
+    while ((car!=EOF) && (car!=' ') && (car!='\t') && (car!='\n') && (car!='\r') && !isdigit(car)) {
         palabra[i++] = car;
         car = fgetc(aStream);
     }
     palabra[i] = '\0';
 }
 
-/*/////////////////////////////////////////////////*/
-
-//esta funcion inicia la lectura del primer car del input y hace un buclesito
-void adquirirPrimeraPalabra()
-{
-    FILE *aStream;
-
-    if ((aStream = fopen("crucigrama.datos","r"))==NULL)
-    {
-        fprintf(stderr,"Error al abrir el archivo\n");
-        exit(2);
+int leerNumero(FILE *aStream) {
+    int valor = 0;
+    while ((car==' ')||(car=='\t')||(car=='\n')||(car=='\r')) {
+        car = fgetc(aStream);
+        if (car == EOF) return -1; // bandera de fin
     }
-    car = fgetc(aStream);
-
-    while (car)
-    {
-        adquirirPalabras (aStream);  //this calls another function
-        if (palabra[0] != '\0')
-        {
-            printf("%s\n", palabra);
-        }
-        else {
-            exit(2);
-        }
-    } 
-    fclose(aStream); 
+    if (!isdigit(car)) return -1;
+    while (isdigit(car)) {
+        valor = valor * 10 + (car - '0');
+        car = fgetc(aStream);
+    }
+    return valor;
 }
 
-
-/*///////////////////////////////////////////////////////////*/
-void funcionTablero(){
-    int f,  //filas
-        c;  //columnas
-
-    for(f=0; f<FILAS; f++){
-        for(c=0; c<COLUMNAS; c++){
-            tablero[f][c].valor = espacio;
-        }
-
+int main() {
+    FILE *aStream = fopen("crucigrama.datos", "r");
+    if (aStream == NULL) {
+        fprintf(stderr, "Error al abrir el archivo\n");
+        exit(1);
     }
 
-
-    T_espacio espacio;
-
-    espacio.sentido=VERTICAL;
-
-
-  if ((tablero = (T_casilla **) malloc (numLineas * sizeof (T_casilla *))) == NULL) {
-      perror ("Malloc tablero err!\n");
-      exit (1);
-   }
-   for (i = 0; i < numLineas; i++)
-      if ((tablero[i] = (T_casilla *) malloc (numColumnas * sizeof (T_casilla))) == NULL) {
-         perror ("Malloc tablero err!\n");
-         exit (2);
-      }
-
-
-//posicion de palabras en el tablero
-//esto esta siendo manual, queremos que adquiera x,y de forma automatica
-//tablero[0][4].valor = vacio;
-
-
-}
-
-/*//////////////////////////////////////////////////*/
-void imprimirTablero()
-{
-    int f,  //filas
-        c;  //columnas
-
-    for (f=0; f < FILAS; f++)
-    {
-        for ( c = 0; c < COLUMNAS; c++)
-        {
-            printf("  %c", tablero[f][c]);
-        }
-        printf("\n");
+    // Leer LINEAS
+    while ((car = fgetc(aStream)) != EOF && !isdigit(car));
+    numLineas = 0;
+    while (isdigit(car)) {
+        numLineas = numLineas * 10 + (car - '0');
+        car = fgetc(aStream);
     }
-}
 
-/*////////////////////////////////////////////////*/
+    // Leer COLUMNAS
+    while ((car = fgetc(aStream)) != EOF && !isdigit(car));
+    numColumnas = 0;
+    while (isdigit(car)) {
+        numColumnas = numColumnas * 10 + (car - '0');
+        car = fgetc(aStream);
+    }
 
-int main()
-{
-    printf("╔═════════════════════════════════════════════╗\n");
-    printf("║            C R U C I G R A M A              ║\n");
-    printf("╚═════════════════════════════════════════════╝\n");
+    tablero = (T_casilla *) malloc(numLineas * numColumnas * sizeof(T_casilla));
+    if (tablero == NULL) {
+        perror("Error al reservar memoria");
+        exit(1);
+    }
 
+    inicializarTablero();
 
-    funcionTablero();
+    // leer coordenadas hasta PALABRAS
+    int leyendoPalabras = 0;
+    while (car != EOF && leyendoPalabras == 0) {
+        adquirirPalabra(aStream);
+        if (strcmp(palabra, "PALABRAS") == 0) {
+            leyendoPalabras = 1;
+        } else {
+            int fila = leerNumero(aStream);
+            while (fila != -1) {
+                int col = leerNumero(aStream);
+                if (col != -1 && fila < numLineas && col < numColumnas) {
+                    tablero[fila * numColumnas + col].valor = VACIO;
+                }
+                fila = leerNumero(aStream);
+            }
+        }
+    }
 
+    // imprimir tablero 
+    printf("╔══════════════════════════════════════╗\n");
+    printf("║          C R U C I G R A M A         ║\n");
+    printf("╚══════════════════════════════════════╝\n");
     imprimirTablero();
 
-    adquirirPrimeraPalabra();
-    //aca verificamos que el archivo inicie con la palabra PALABRAS
-
-    if(strcmp(palabra,"PALABRAS")!=0)
-    {   
-        fprintf(stderr,"Error: PALABRAS del crucigrama no encontradas\n");    //de lo contrario imprime un error
-        exit(1);
-    } else {
-        printf("\nPALABRAS: %s\n", palabra);
+    // leer e imprimir palabras
+    printf("╔═════════════════════╗\n");
+    printf("║   P A L A B R A S   ║\n");
+    printf("╚═════════════════════╝\n");
+    while (car != EOF) {
+        adquirirPalabra(aStream);
+        if (palabra[0] != '\0') {
+            printf("%s\n", palabra);
+        }
     }
 
+    fclose(aStream);
     return 0;
 }
